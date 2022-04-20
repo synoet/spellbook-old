e459ebd00f595dcf429c0e4750b197d03339139d
synoet <teonys@nyu.edu>
2022-04-20 16:31:49 -0400

    cli: remove popup logic

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -256,32 +256,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_y)/2),
-                Constraint::Percentage(percent_y),
-                Constraint::Percentage((100 - percent_y)/2),
-
-            ]
-            .as_ref(),
-        )
-        .split(r);
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints(
-            [
-                Constraint::Percentage((100 - percent_x)/ 2),
-                Constraint::Percentage(percent_x),
-                Constraint::Percentage((100 - percent_x)/2),
-            ]
-            .as_ref(),
-        )
-        .split(popup_layout[1])[1]
-}
-
 fn command_rank_sort(commands: Vec<LocalCommand>, query: &str) -> Vec<LocalCommand> {
     struct RankedCommand {
         command: LocalCommand,
